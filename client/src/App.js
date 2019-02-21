21c6c11bd827bb4000566902501d6bde587093de
Erick Pacheco Pedraza <eum602@gmail.com>
2019-02-21 17:00:12 -0500

    change title

--- client/src/App.js
+++ client/src/App.js
@@ -13,17 +13,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+            <h1 className="App-title">Fib calculator</h1>            
             <Link to="/">Home</Link>
             <Link to="/otherPage">Other Page</Link>
           </header>
