823242fbae5dfb84dc6e06630cb8ebf41c4edb6b
Erick Pacheco Pedraza <eum602@gmail.com>
2019-02-27 14:57:46 -0500

    modified some issues 1

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,11 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib calculator</h1>            
             <Link to="/">Home</Link>
-            <Link to="/otherPage">Other Page</Link>
+            <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component = {Fib}/>
-            <Route path="/otherPage" component={otherPage}/>
+            <Route path="/otherpage" component={otherPage}/>
           </div>
         </div>
       </Router>
